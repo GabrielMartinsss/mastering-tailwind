dcd150e9967756006f937a524bac95d12f4c177d
Gabriel Martins <gabrielsilva3553@gmail.com>
2023-12-29 09:07:09 -0300

    feat: create Sidebar components

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,26 @@
+import { LogOut } from 'lucide-react'
+
+export function Profile() {
+  return (
+    <div className="grid-cols-profile grid items-center gap-3">
+      <img
+        src="http://github.com/gabrielmartinsss.png"
+        className="h-10 w-10 rounded-full"
+        alt=""
+      />
+
+      <div className="flex flex-1 flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700">
+          Gabriel Martins
+        </span>
+        <span className="truncate text-sm text-zinc-500">
+          gabrielsilva3553@gmail.com
+        </span>
+      </div>
+
+      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
+    </div>
+  )
+}
