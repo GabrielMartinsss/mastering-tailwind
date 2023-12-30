fe517532bffe25edf95e7cb8f5f670d2949410d8
Gabriel Martins <gabrielsilva3553@gmail.com>
2023-12-30 07:28:18 -0300

    feat: create Tab component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import { SettingTabs } from '@/components/SettingsTabs'
+
 export default function Home() {
   return (
-    <h1>
-      hello word this is a message very huge because a need fill the space in
-      vscode editor to do break line to test eslint config
-    </h1>
+    <>
+      <SettingTabs />
+    </>
   )
 }
