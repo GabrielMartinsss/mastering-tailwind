f942e47fbdfbbd06373069e8f7430e0e4813286d
Gabriel Martins <gabrielsilva3553@gmail.com>
2024-01-04 09:49:50 -0300

    style: remove unnecessary margin

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -22,7 +22,7 @@ type InputRootProps = ComponentProps<'div'>
 export function Root(props: InputRootProps) {
   return (
     <div
-      className="mx-1 flex items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm focus-within:border-zinc-500"
+      className="flex items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm focus-within:border-zinc-500"
       {...props}
     />
   )
