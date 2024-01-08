f95992ae51c06e03cbaeea1ae751daa8a2d44968
Gabriel Martins <gabrielsilva3553@gmail.com>
2024-01-08 09:31:26 -0300

    style: add dark mode on Sidebar

--- src/components/Sidebar/NavItem.tsx
+++ src/components/Sidebar/NavItem.tsx
@@ -1,6 +1,7 @@
 import { ChevronDown } from 'lucide-react'
 import Link from 'next/link'
 import { ElementType } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 interface NavItemProps {
   title: string
@@ -11,13 +12,18 @@ export function NavItem({ title, icon: Icon }: NavItemProps) {
   return (
     <Link
       href=""
-      className="group flex items-center gap-3 rounded px-3 py-2 outline-none hover:bg-violet-50 focus:border focus-visible:border-violet-300 focus-visible:ring-2 focus-visible:ring-violet-100"
+      className={twMerge(
+        'group flex items-center gap-3 rounded px-3 py-2',
+        'outline-none hover:bg-violet-50 focus:border dark:hover:bg-zinc-800',
+        'focus-visible:border-violet-300 focus-visible:ring-2 focus-visible:ring-violet-100 ',
+        'dark:focus-within:border-violet-500 dark:focus-within:ring-violet-500/20',
+      )}
     >
-      <Icon className="h-5 w-5 text-zinc-500" />
-      <span className="font-medium text-zinc-700 group-hover:text-violet-500">
+      <Icon className="h-5 w-5 text-zinc-500 dark:text-zinc-600" />
+      <span className="font-medium text-zinc-700 group-hover:text-violet-500 dark:text-zinc-100 dark:group-hover:text-violet-300">
         {title}
       </span>
-      <ChevronDown className="ml-auto h-5 w-5 text-zinc-400 group-hover:text-violet-300" />
+      <ChevronDown className="ml-auto h-5 w-5 text-zinc-400 group-hover:text-violet-300 dark:text-zinc-600" />
     </Link>
   )
 }
--- src/components/Sidebar/UsedSpaceWidget.tsx
+++ src/components/Sidebar/UsedSpaceWidget.tsx
@@ -1,27 +1,39 @@
+import { twMerge } from 'tailwind-merge'
+
 export function UsedSpaceWidget() {
   return (
-    <div className="flex flex-col gap-4 rounded-lg bg-violet-50 px-4 py-5">
+    <div className="flex flex-col gap-4 rounded-lg bg-violet-50 px-4 py-5 dark:bg-zinc-800">
       <div className="space-y-1">
-        <span className="text-sm/5 text-violet-700">Used space</span>
-        <p className="text-sm/5 text-violet-500">
+        <span className="text-sm/5 text-violet-700 dark:text-zinc-100">
+          Used space
+        </span>
+        <p className="text-sm/5 text-violet-500 dark:text-zinc-400">
           Your team has used 80% of your available space. Need more?
         </p>
       </div>
 
-      <div className="h-2 rounded-full bg-violet-200">
-        <div className="h-2 w-4/5 rounded-full bg-violet-600"></div>
+      <div className="h-2 rounded-full bg-violet-200 dark:bg-zinc-600">
+        <div className="h-2 w-4/5 rounded-full bg-violet-600 dark:bg-violet-400"></div>
       </div>
 
       <div className="space-x-3">
         <button
           type="button"
-          className="rounded text-sm font-medium text-violet-500 outline-none hover:text-violet-700 focus-visible:ring-2 focus-visible:ring-violet-400 focus-visible:ring-offset-4"
+          className={twMerge(
+            'rounded text-sm font-medium text-violet-500 outline-none',
+            'hover:text-violet-700 focus-visible:ring-2 focus-visible:ring-violet-400 focus-visible:ring-offset-1',
+            ' dark:text-violet-300 dark:hover:text-violet-200 dark:focus-visible:ring-violet-600',
+          )}
         >
           Dismiss
         </button>
         <button
           type="button"
-          className="rounded text-sm font-medium text-violet-700 outline-none hover:text-violet-900 focus-visible:ring-2 focus-visible:ring-violet-400 focus-visible:ring-offset-4"
+          className={twMerge(
+            'rounded text-sm font-medium text-violet-700 outline-none',
+            'hover:text-violet-900 focus-visible:ring-2 focus-visible:ring-violet-400 focus-visible:ring-offset-1',
+            ' dark:text-violet-500 dark:hover:text-violet-300 dark:focus-visible:ring-violet-600',
+          )}
         >
           Upgrade plan
         </button>
