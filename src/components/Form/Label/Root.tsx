e9e34d7ad8ee4edf04aa2208f6e3549e9d8c8a6a
Gabriel Martins <gabrielsilva3553@gmail.com>
2024-01-08 08:33:38 -0300

    style: add variants on Label component

--- src/components/Form/Label/Root.tsx
+++ src/components/Form/Label/Root.tsx
@@ -1,7 +1,23 @@
 import { ComponentProps } from 'react'
+import { VariantProps, tv } from 'tailwind-variants'
 
-export type RootProps = ComponentProps<'label'>
+const label = tv({
+  base: 'text-sm font-medium text-zinc-700',
 
-export function Root(props: RootProps) {
-  return <label {...props} className="text-sm font-medium text-zinc-700" />
+  variants: {
+    variant: {
+      primary: '',
+      mobile: 'lg:sr-only',
+    },
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+  },
+})
+
+export type RootProps = ComponentProps<'label'> & VariantProps<typeof label>
+
+export function Root({ variant, className, ...props }: RootProps) {
+  return <label className={label({ variant, className })} {...props} />
 }
